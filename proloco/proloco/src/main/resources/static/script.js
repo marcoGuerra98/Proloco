09ca8fc1e71f5a87c9b6f12deaf2a7bcfcc9b6c9
MarcoGuerra <marco.guerra@finconsgroup.com>
2025-01-29 23:26:29 +0100

    aggiunti nuovi campi in js

--- proloco/proloco/src/main/resources/static/script.js
+++ proloco/proloco/src/main/resources/static/script.js
@@ -4,16 +4,23 @@ document.getElementById('dataForm').addEventListener('submit', function(event) {
     const anagrafica = {
         nome: document.getElementById('nome').value,
         cognome: document.getElementById('cognome').value,
+        sesso: document.getElementById('sesso').value,
+        caricaSociale: document.getElementById('caricaSociale').value,
+        dataIscrizione: document.getElementById('dataIscrizione').value,
         codiceFiscale: document.getElementById('codiceFiscale').value,
         dataNascita: document.getElementById('dataNascita').value,
-        indirizzo: document.getElementById('indirizzo').value,
-        numeroCivico: document.getElementById('numeroCivico').value
+        comuneNascita: document.getElementById('comuneNascita').value,
+        statoEsteroNascita: document.getElementById('statoEsteroNascita').value,
+        comuneEsteroNascita: document.getElementById('comuneEsteroNascita').value,
+        comune: document.getElementById('cognome').value,
+        indirizzo: document.getElementById('indirizzo').value
     };
 
     const user = {
         anagrafica: anagrafica,
         email: document.getElementById('email').value,
         numeroCellulare: document.getElementById('numeroCellulare').value
+        statoIscrizione: document.getElementById('statoIscrizione').value
     }
 
     fetch('/addListToDB', {
